8b27405c821c0c98e71f8d428e7566cffb9ad4d6
Ivaylo Arnaudov <arnaudoff@users.noreply.github.com>
2017-04-20 16:06:54 +0300

    Implement next and prev buttons

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import tkinter as tk
+import Tkinter as tk
+# import tkinter as tk <= for Python 3
 import sys
 from PIL import ImageTk, Image
 
 class Startup:
 
     def on_button_click(self, root_window):
-      file_win = tk.Toplevel(root_window)
-      button = tk.Button(file_win, text="Do nothing button")
-      button.pack()
+        file_win = tk.Toplevel(root_window)
+        button = tk.Button(file_win, text="Do nothing button")
+        button.pack()
+    
+    def on_next_button_click(self):
+	pass
+	
+    def on_prev_button_click(self):
+	pass
 
     def setup_menubar(self, window):
         menu_bar = tk.Menu(window)
@@ -53,6 +60,16 @@ class Startup:
         gallery_window.window.title("Advanced Photo Viewer")
         gallery_window.window.configure(background='white')
 
+        prev_button = tk.Button(
+		gallery_window.window,
+		text="Prev",
+		command=lambda: self.on_prev_button_click,
+		bg="lightblue",
+                relief=tk.FLAT)
+	prev_button.pack(padx=250, pady=10, side=tk.LEFT)
+
+	# Image related stuff
+
         path = 'meme.jpg'
 
         # Creates a Tkinter-compatible photo image, which can be used everywhere
@@ -62,9 +79,15 @@ class Startup:
         # The Label widget is a standard Tkinter widget used to display a
         # text or image on the screen.
         panel = tk.Label(gallery_window.window, image = image)
+	panel.pack(padx=5, pady=20, side=tk.LEFT)
 
-        # The Pack geometry manager packs widgets in rows or columns.
-        panel.pack(side = "bottom", fill = "both", expand = "yes")
+        next_button = tk.Button(
+		gallery_window.window,
+		text="Next",
+		command=lambda: self.on_next_button_click,
+		bg="lightblue",
+		relief=tk.FLAT)
+	next_button.pack(padx=250, pady=20, side=tk.LEFT)
 
         self.setup_menubar(gallery_window.window)
 
